28e0e3ac6f81fcf57539f2de505fb161c081d112
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 19:42:03 -0300

    [M1S08] EX1 - Projeto inicial

--- src/main/java/com/senai/gerenciamentoalunos/GerenciamentoAlunosApplication.java
+++ src/main/java/com/senai/gerenciamentoalunos/GerenciamentoAlunosApplication.java
@@ -8,6 +8,6 @@ public class GerenciamentoAlunosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GerenciamentoAlunosApplication.class, args);
-    }
 
+    }
 }
