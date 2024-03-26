6b68da843ebf989871868ce7c3accf694bcb2427
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 20:56:16 -0300

    [M1S08] EX2 - Alunos

---
+++ src/main/java/com/senai/gerenciamentoalunos/model/AlunoModel.java
@@ -0,0 +1,22 @@
+package com.senai.gerenciamentoalunos.model;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Data
+public class AlunoModel {
+
+    private static Integer proximoId = 1;
+
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+    private String nome;
+    private LocalDate dataNascimento;
+
+    private Integer getProximoId(){
+        return proximoId++;
+    }
+
+}
