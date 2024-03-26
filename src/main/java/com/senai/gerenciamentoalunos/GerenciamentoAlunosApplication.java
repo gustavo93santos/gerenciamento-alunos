35e3e8441f644ba7de78aba5d24018133bdf7693
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 19:38:58 -0300

    Initial commit

---
+++ src/main/java/com/senai/gerenciamentoalunos/GerenciamentoAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.senai.gerenciamentoalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAlunosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoAlunosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/senai/gerenciamentoalunos/GerenciamentoAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.gerenciamentoalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoAlunosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
