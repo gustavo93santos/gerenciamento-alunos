fda78640615efdcd98f95bd26942635810d8e4b9
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-03-26 22:14:23 -0300

    [M1S08] EX6 - AlunoController

--- src/main/java/com/senai/gerenciamentoalunos/service/CursoService.java
+++ src/main/java/com/senai/gerenciamentoalunos/service/CursoService.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Service
 public class CursoService {
 
-    public List<CursoModel> buscarAlunos(){
+    public List<CursoModel> buscarCursos(){
         return CursoModel.getCursos();
     }
 
